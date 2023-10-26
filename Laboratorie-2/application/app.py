ad8d8d31a891c1662d24dabfc5f8c00e834eb248
Navid <nav.khosravi@gmail.com>
2023-10-26 06:27:00 +0200

    fortsätter experimentera

--- Laboratorie-2/application/app.py
+++ Laboratorie-2/application/app.py
@@ -6,19 +6,49 @@ app = Flask(__name__)
 
 @app.route("/")
 def first_page():
-    jinja_2_test = "jinja2 har infogats på första sidan"
-    return render_template("layout.html", data=jinja_2_test)
+    bullshit = "Hello world"
+    return render_template("layout.html", data_0=bullshit)
 
 
-@app.route("/andra_sida")
+@app.route("/second")
 def second_page():
-    return render_template("layout.html", header="Detta är en header")
+    lokal_variabel = "Lokal variabel, den är innanför funktionen"
+    return render_template("layout.html", data_1=lokal_variabel)
 
-@app.route("/tredje_sida")
+
+global_variabel = "Här används en global variabel, den är utanför funktionen."
+@app.route("/third")
 def third_page():
+    return render_template("layout.html", data_2=global_variabel)
+
+
+@app.route("/fourth")
+def fourth_page():
+    bullshit = "Detta är fjärde sidan"
+    return render_template("layout.html", data_3=bullshit)
+
+
+@app.route("/fifth")
+def fifth_page():
+    favorit_glass = ["Vanilj", "Chocolad", "Citron"]
+    rubrik = "Favoritglassar: "
+    line = "-" * 30
+    return render_template("layout.html", 
+                           rubrik=rubrik, 
+                           favorit_glass=favorit_glass, 
+                           line=line)
+
+
+@app.route("/sixth")
+def sixth_page():
+    line = "-" * 30
+    return render_template("layout.html",
+                           line=line)
+    
+
+@app.route("/seventh")
+def seventh_page():
     return render_template("layout.html")
-
-
 
 
 app.run(debug=True)

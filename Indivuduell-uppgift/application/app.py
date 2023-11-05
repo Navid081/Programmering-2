c2495c1c0bc35b13cac916abb6addc1fa70f83ac
Navid <nav.khosravi@gmail.com>
2023-11-05 16:59:55 +0100

    Kommit en liten bit påvägen

---
+++ Indivuduell-uppgift/application/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+# Båda endpoints tar oss till samma funktion
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

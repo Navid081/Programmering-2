518077205505f56f0f77a24b82d87e8e75693fb5
Navid <nav.khosravi@gmail.com>
2023-11-07 22:05:43 +0100

    la till en function som jag skapade

---
+++ Indivuduell-uppgift/application/func.py
@@ -0,0 +1,14 @@
+# Kan jag ändra kolumnerna enklare?
+# Behövs detta ens? Jag kan göra detta i endpoint funktionen.
+def slice_times(DataFrame, old_column_name, new_column_name):
+    """Function takes a pandas DataFrame column and slices it
+    Args:
+        DataFrame (_type_): Pandas DataFrame
+        old_column_name (_type_): Name of column one wish to change
+        new_column_name (_type_): Name of the new modified column
+
+    Returns:
+        _type_: _Returns new modified column_
+    """
+    DataFrame[new_column_name] = DataFrame[old_column_name].str[11:16]    # Omvandla kolumnen till en sträng och slica den.
+    return DataFrame

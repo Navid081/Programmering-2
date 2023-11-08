27fa8772f68a87830a9cd6f02cfc8b593699182c
Navid Khosravi <nav.khosravi@gmail.com>
2023-11-08 21:50:06 +0100

    Färdig

--- Indivuduell-uppgift/application/func.py
+++ Indivuduell-uppgift/application/func.py
@@ -1,18 +1,30 @@
-# Här kommer alla funktioner jag skapar att finnas.
-
+import pandas as pd
+import requests
+import json
 
 
 
 def slicing_iso_8601(DataFrame, old_column_name, new_column_name):
-    """Function takes a pandas DataFrame column and slices column, creates a new column, and deletes the old one.
+    """Function takes a pandas column, slices iso-8601 to hh:mm, creates a new column, and deletes the old one.
     Args:
         DataFrame (_type_): Pandas DataFrame
-        old_column_name (_type_): Name of column one wish to change
-        new_column_name (_type_): Name of the new modified column
-
+        old_column_name (_type_): Name of existing column
+        new_column_name (_type_): Name of the new column
     Returns:
-        _type_: _Returns new modified column_
-    """
-    DataFrame[new_column_name] = DataFrame[old_column_name].str[11:16]    # Omvandla kolumnen till en sträng och slica den.
-    del DataFrame[old_column_name]                                        # Tar bort den gamla tabellen som har slicats
-    return DataFrame
+        _type_: _Returns new modified column_"""
+    DataFrame[new_column_name] = DataFrame[old_column_name].str[11:16]  # Slicar bort allt förutom hh:mm.
+    del DataFrame[old_column_name]                                      # Tar bort gamla DataFrame.
+    return DataFrame                                                    # Returnerar nya DataFrame.
+
+
+def api_url_to_pandas_dataframe(url):
+    """Tar en API-url och konverterar den till en pandas DataFrame.
+    Args:
+        url (str): URL till API: n.
+    Returns:
+        pd.DataFrame: En DataFrame med data från API: n."""
+    response = requests.get(url)                    # Hämtar data från URL:en.
+    response_string = response.text                 # Konverterar svaret till en sträng.
+    response_list = json.loads(response_string)     # Omvandlar strängen till en Python-lista.
+    df = pd.DataFrame(response_list)                # Skapar en pandas DataFrame från listan.
+    return df                                       # Returnerar den skapade DataFramen.

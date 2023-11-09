6a2e4dc1eebe39336a2c928f41348b6c8bda0436
Navid Khosravi <nav.khosravi@gmail.com>
2023-11-10 00:38:24 +0100

    ändrade filnamnet, kommenterat

--- Indivuduell-uppgift/tests/test_functions.py
+++ Indivuduell-uppgift/tests/test_functions.py
@@ -1,8 +1,8 @@
-# Test case mot kod
+# Test mot kod.
 
 import pytest
 
-# Gör ett test för att kontrollera att vissa rubriker är med.
+# Gör ett test för att kontrollera att kolumnerna "time_start" och "time_end" är med i 
 
 
 # Gör ett test för att kontrollera att tiderna är angivna i iso-8601 eller i hh:mm format.
